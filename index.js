e562857f8074c54c6bf4cc043d61af3d65e31bdb
Seong Chang <seongbo5230@gmail.com>
2020-08-08 23:37:58 -0700

    adding menu templates

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const electron = require("electron");
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
 app.on("ready", () => {
   mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/main.html`)
+
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
 });
+
+// Menu template
+const menuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      { label: 'New Todo' },
+      {
+        label: 'Quit',
+        accelerator: '',
+        click() {
+          app.quit();
+        }
+      }
+    ]
+  }
+];
+
+if (process.platform === 'darwin') {
+  menuTemplate.unshift({});
+}
