be9bbb3f47e28c210145cb0e02f438eda9128ae9
Seong Chang <seongbo5230@gmail.com>
2020-08-09 16:47:53 -0700

    todo function added + garbage collector + keyboard shortcuts

--- index.js
+++ index.js
@@ -1,34 +1,90 @@
 const electron = require("electron");
 
-const { app, BrowserWindow, Menu } = electron;
+const { app, BrowserWindow, Menu, ipcMain } = electron;
 
 let mainWindow;
+let addWindow;
 
+// Main initial startup of the app
 app.on("ready", () => {
-  mainWindow = new BrowserWindow({});
-  mainWindow.loadURL(`file://${__dirname}/main.html`)
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    title: "Todotron",
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  mainWindow.loadURL(`file://${__dirname}/main.html`);
+  mainWindow.on("closed", () => app.quit());
 
   const mainMenu = Menu.buildFromTemplate(menuTemplate);
   Menu.setApplicationMenu(mainMenu);
 });
 
+// Function that adds new todo window
+function createAddWindow() {
+  addWindow = new BrowserWindow({
+    width: 500,
+    height: 300,
+    title: "Todotron - Add New Todo",
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  addWindow.loadURL(`file://${__dirname}/add.html`);
+  addWindow.on("closed", () => (addWindow = null));
+}
+
+// event handler on clicking "add"
+ipcMain.on("todo:add", (event, todo) => {
+  mainWindow.webContents.send("todo:add", todo);
+  addWindow.close();
+});
+
 // Menu template
 const menuTemplate = [
   {
-    label: 'File',
+    label: "File",
     submenu: [
-      { label: 'New Todo' },
       {
-        label: 'Quit',
-        accelerator: '',
+        label: "New Todo",
+        accelerator: process.platform === "darwin" ? "Command+N" : "Ctrl+N",
+        click() {
+          createAddWindow();
+        },
+      },
+      {
+        label: "Quit",
+        accelerator: process.platform === "darwin" ? "Command+Q" : "Ctrl+Q",
         click() {
           app.quit();
-        }
-      }
-    ]
-  }
+        },
+      },
+    ],
+  },
 ];
 
-if (process.platform === 'darwin') {
+// Checks
+if (process.platform === "darwin") {
   menuTemplate.unshift({});
+}
+
+if (process.env.NODE_ENV !== "production") {
+  menuTemplate.push({
+    label: "View",
+    submenu: [
+      {
+        role: "reload",
+      }
+      {
+        label: "Toggle Developer Tools",
+        accelerator: process.platform === "darwin" ? "Command+Alt+I" : "Ctrl+Shift+I",
+        click(item, focusedWindow) {
+        focusedWindow.toggleDevTools();
+        },
+      },
+    ],
+  });
 }
